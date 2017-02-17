348517c192934931b45d27f04194f650416b2e3f
Jenny Chu <JennyChu@Jennys-MacBook-Pro.local>
2017-02-16 18:09:14 -0800

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,20 +5,28 @@ var game = {
 	questionCount: 0,
 	myTimer: null,
 	currentAns: null,
+    firstClick: true,
+    choices: [],
 
 	newQuestion: function() {
-		// clear and restart timer
-		this.clearTimer();
+		// make sure user doesn't click multiple times per question
+        this.firstClick = true;
+        // remove highlights
+        $(".choices").removeClass("correctAns selectedAns");
+        // clear announcements
+        $("#displayAnnounce").html("");
+		// restart timer
 		this.startTimer();
 		// reset answer options background color
-		$(".choices").css("background-color","#fff")
+		// $(".choices").css("background-color","#fff")
 		// pick new question
 		questionObj = availableQuestions[this.questionCount];
 		this.questionCount++;
 		console.log(questionObj);
 
 		// store answer
-		currentAns = questionObj.answer;
+		this.currentAns = questionObj.answer;
+        console.log("Current answer is: " + this.currentAns);
 
 		// display question and answers of question picked
 		$("#displayQuestion").html(questionObj.question);
@@ -36,14 +44,8 @@ var game = {
 			game.remainingTime -= 1;
 			$("#timeLeft").html(game.remainingTime);
 			if(game.remainingTime === 0) {
+                game.firstClick = false;
 				game.outOfTime();
-				// HIGHLIGHT BACKGROUND OF CORRECT ANSWER
-				for (var i=0; i < questionObj.options.length; i++) {
-					if (currentAns === questionObj.options[i]) {
-						var correctAns = $(".answerChoices").children.eq(0).children.eq(i);
-						
-					};
-				};
 			};
 		}, 1000)
 		
@@ -51,6 +53,7 @@ var game = {
 
 	clearTimer: function() {
 		clearInterval(game.myTimer);
+        $("#timeLeft").html(0);
 	},
 
 	winGame: function() {
@@ -59,9 +62,12 @@ var game = {
 		$("#winsCount").html(this.winsCount);
 		// announce you won
 		$("#displayAnnounce").html("You're a wizard Harry!");
-		// start new question
-		// DELAY THE CALL OF THIS FUNCTION
-		this.newQuestion();
+		// highlight correct answer
+        this.highlightAnswer();
+		// start new question with 2 second delay
+		setTimeout(function(){
+            game.newQuestion();
+        }, 2000);
 	},
 
 	loseGame: function() {
@@ -69,10 +75,13 @@ var game = {
 		this.losesCount += 1;
 		$("#losesCount").html(this.losesCount);
 		// announce you lost
-		$("#displayAnnounce").html("Whomp whomp");
-		// start new question
-		// DELAY THE CALL OF THIS FUNCTION
-		this.newQuestion();
+		$("#displayAnnounce").html("Whomp whomp!");
+        // highlight correct answer
+        this.highlightAnswer();
+		// start new question with 2 second delay
+		setTimeout(function(){
+            game.newQuestion();
+        }, 2000);
 	},
 
 	outOfTime: function() {
@@ -80,10 +89,27 @@ var game = {
 		this.losesCount += 1;
 		$("#losesCount").html(this.losesCount);
 		// announce out of time
-		$("#displayAnnounce").html("Out of time!");
-		// start new question
-		this.newQuestion();
+        $("#displayAnnounce").html("Out of time!");
+        this.clearTimer();
+        // highlight correct answer
+        this.highlightAnswer();
+        // start new question with 2 second delay
+        setTimeout(function(){
+            game.newQuestion();
+        }, 2000);
 	},	
+    
+    highlightAnswer: function() {
+        // select choices that are available
+        choices = $("#answerChoices").children();
+        // highlight the correct answer
+        choices.each(function() {
+        	console.log($(this).html());
+        	if ($(this).html() === game.currentAns) {
+        		$(this).addClass("correctAns");
+        	}
+        });
+   },
 };
 
 var availableQuestions = [
@@ -103,9 +129,9 @@ var availableQuestions = [
     options: ["1987", "1985", "1981", "1980"]
   },
   {
-    question: "What was the last name of Professor Trelawney's former husband?",
+    question: "What's Professor Trelawney's former husband's last name?",
     answer: "Higglebottom",
-    options: ["Stroker", "Peucey", "Higglebottom", "Niggemeyer"]
+    options: ["Stroker", "Peucey", "Higglebottom", "Nigglemeyer"]
   },
   {
     question: "Who is only a day older than Harry?",
@@ -120,7 +146,7 @@ var availableQuestions = [
   {
     question: "What do cats do to Hagrid?",
     answer: "Make him sneeze",
-    options: ["Make him angry", "Cause him pain", "Make him lauch", "Make him sneeze"]
+    options: ["Make him angry", "Cause him pain", "Make him cough", "Make him sneeze"]
   },
   {
     question: "What is Hermoine Granger's Patronus?",
@@ -132,36 +158,92 @@ var availableQuestions = [
     answer: "Mars Bars",
     options: ["Kit Kats", "Mars Bars", "Hershey Bars", "Snickers"]
   },
+  {
+    question: "What does Molly like Arthur to call her in private?",
+    answer: "Mollywobbles",
+    options: ["Dearest", "Mollybabe", "Mollywobble", "Honey"]
+  },
+  {
+    question: "How many languages has the Harry Potter book series been translated into?",
+    answer: "67",
+    options: ["67", "45", "39", "72"]
+  },
+  {
+    question: "How many times was Celetina Warbeck married?",
+    answer: "3",
+    options: ["5", "3", "1", "8"]
+  },
+  {
+    question: "What did Professor Quirrel teach before Defense Against the Dark Arts?",
+    answer: "Muggle Studies",
+    options: ["Ancient Runes", "Divination", "Muggle Studies", "Alchemy"]
+  },
+  {
+    question: "What form does Rita Skeeter take as an Animagus?",
+    answer: "Beetle",
+    options: ["Squirrel", "Mouse", "Beetle", "Bird"]
+  },
+  {
+    question: "What is Harry Potter's birthday?",
+    answer: "July 31",
+    options: ["May 31", "July 30", "July 31", "August 1"]
+  },
+  {
+    question: "What is Hermoine Granger's middle name?",
+    answer: "Jean",
+    options: ["Jane", "Jean", "Joan", "Jeannie"]
+  },
+  {
+    question: "Which Hogwarts house was Gilderoy Lockhart in?",
+    answer: "Ravenclaw",
+    options: ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+  },
+  {
+    question: "How many victims were petrified by the Basilisk in Chamber of Secrets?",
+    answer: "Six",
+    options: ["Five", "Six", "Seven", "Nine"]
+  },
+  {
+    question: "What is Moaning Myrtle's surname?",
+    answer: "Warren",
+    options: ["Jones", "Warren", "Mitchell", "Smith"]
+  },
 ]
 
 $("#startGame").on("click", function() {
-	// call newQuestion function
-	game.newQuestion();
+    // call newQuestion function
+    game.newQuestion();
 
-	// change visibility of question and answers to visible
-	$("#displayQuestion").css("visibility","visible");
-	$(".choices").css("visibility","visible");
+    // change visibility of question and answers to visible
+    $("#displayQuestion").css("visibility","visible");
+    $(".choices").css("visibility","visible");
 
-	// change visibility of Start Game button to hidden
-	$("#startGame").css("visibility","hidden");
+    // change visibility of Start Game button to hidden
+    $("#startGame").css("visibility","hidden");
 
 });
 
 $(".choices").on("click", function() {
-	// store user choice
-	var userChoice = $(this).text();
-	console.log("User choice: " + userChoice);
-
-	// check if user guess is correct
-	for (var i=0; i < availableQuestions.length; i++) {
-		if (userChoice === questionObj.answer) {
-			// user wins
-			game.winGame();
-			return
-		} else {
-			// user loses
-			game.loseGame();
-			return
-		}
-	}
+    if(game.firstClick === true) {
+        game.firstClick = false;
+        // store user choice
+        var userChoice = $(this).text();
+        console.log("User choice: " + userChoice);
+        // check if user guess is correct
+        for (var i=0; i < availableQuestions.length; i++) {
+            if (userChoice === questionObj.answer) {
+                // user wins
+                game.clearTimer();
+                game.winGame();
+                return
+            } else {
+            	// highlight user choice
+        		$(this).addClass("selectedAns");
+                // user loses
+                game.clearTimer();
+                game.loseGame();
+                return
+            }
+        }
+    };
 });
